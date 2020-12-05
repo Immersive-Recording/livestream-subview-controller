714e34242656c3d8f1c0a18d5ade53ae000c5ec0
Joshua Cline <wwjdtd@gmail.com>
2020-12-05 10:08:23 -0800

    This finally works.

--- server.ts
+++ server.ts
@@ -6,24 +6,43 @@ import {
 } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { assert } from "https://deno.land/std@0.79.0/testing/asserts.ts";
 
-const bundles = {
-    test: "",
-}
+const compiled: Map<string, string> = new Map<string, string>();
 {
-    console.log("Bundling test...")
-    const [diagnostics, emit] = await Deno.bundle(`${Deno.cwd()}/client/script/test.ts`);
-    assert(diagnostics == null);
-    bundles.test = emit;
+    console.log("Compiling test...")
+    const [diagnostics, emitMap] = await Deno.compile(
+        `./client/script/test.ts`,
+        undefined,
+        {
+            sourceMap: false,
+            inlineSourceMap: true,
+        }
+        );
+    assert(diagnostics == null, `Compile Error: ${JSON.stringify(diagnostics)}`);
+    const cwd = `file://${Deno.cwd()}/client/script/`;
+    for(const resource in emitMap){
+        compiled.set(resource.replace(cwd, ""), emitMap[resource]);
+    }
+}
+
+{
+    console.log("The following have been re-compiled...");
+    for(const k of compiled.keys()){
+        console.log(k);
+    }
 }
 
 const app = new Application();
 const router = new Router();
 app.use(router.routes());
 app.use(router.allowedMethods());
-router.get("/bundles/test.js", (context: Context) => {
-    context.response.body = bundles.test;
-    context.response.type = "application/javascript";
-});
+router.get("/compiled/:script", (context) => {
+    console.log(context.params?.script);
+    if (context.params?.script && compiled.has(context.params.script)) {
+        console.log(compiled.has(context.params.script))
+        context.response.body = compiled.get(context.params.script);
+        context.response.type = "text/javascript";
+    }
+})
 router.get("/", async (context: Context) => {
     await send(context, context.request.url.pathname, {
         root: `${Deno.cwd()}/client`,
