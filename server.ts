86a3a8174e0a64e0bae687c762d5b6157feba7a9
Joshua Cline <wwjdtd@gmail.com>
2020-12-05 10:32:34 -0800

    Moving on to more important things...

--- server.ts
+++ server.ts
@@ -5,23 +5,31 @@ import {
     send
 } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { assert } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import { parse } from "https://deno.land/std@0.79.0/flags/mod.ts";
+const parsedArgs = parse(Deno.args);
 
 const compiled: Map<string, string> = new Map<string, string>();
-{
+if(parsedArgs.r){
     console.log("Compiling test...")
     const [diagnostics, emitMap] = await Deno.compile(
-        `./client/script/test.ts`,
-        undefined,
-        {
-            sourceMap: false,
-            inlineSourceMap: true,
-        }
-        );
+            `./client/script/test.ts`,
+            undefined,
+            {
+                sourceMap: false,
+                inlineSourceMap: true,
+            }
+            );
     assert(diagnostics == null, `Compile Error: ${JSON.stringify(diagnostics)}`);
     const cwd = `file://${Deno.cwd()}/client/script/`;
     for(const resource in emitMap){
         compiled.set(resource.replace(cwd, ""), emitMap[resource]);
     }
+} else {
+    //This is a problem, but It will wait for refactor to fix.
+    const cache: Record<string, string> = JSON.parse(await Deno.readTextFile("./cache/compiled.json"));
+    for (const value in cache) {
+        compiled.set(value, cache[value])
+    }  
 }
 
 {
@@ -29,6 +37,14 @@ const compiled: Map<string, string> = new Map<string, string>();
     for(const k of compiled.keys()){
         console.log(k);
     }
+}
+{
+    const cache: Record<string, string> = {};
+    compiled.forEach((value, key) => {
+        cache[key] = value
+    });
+    await Deno.writeTextFile("./cache/compiled.json", JSON.stringify(cache));
+    console.log("Cache updated.");
 }
 
 const app = new Application();
